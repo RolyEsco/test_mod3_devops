efdb665c64da60f548380500e3671c22515ba77b
roly.esco <roly.esco@gmail.com>
2022-02-13 22:39:50 -0400

    Aplicacion terminada con plugins y resources

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,9 +2,26 @@ package com.example;
 
 //import static spark.Spark.*;
 import static spark.Spark.get;
+import static spark.Spark.port;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 
 public class App {
-    public static void main(String[] args) {
-        get("/entornos", (req, res) -> "En el ambiente ");
+
+    private static final String PROPIEDAD_AMBIENTE = null;
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String perfil;
+        port(3456);
+        Properties propiedades = new Properties();
+        propiedades.load(new FileInputStream(new File("src/main/resources/vars.properties")));
+        perfil=propiedades.getProperty("PROPIEDAD_AMBIENTE");
+        get("/entornos", (req, res) -> "En el ambiente <"+perfil+">");
     }
+    
 }
